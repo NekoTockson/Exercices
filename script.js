ce3987a4d561a8386d5d9abb2146c708e2ea1043
Alexandre Tanguay <2383528@collegemv.qc.ca>
2024-02-13 18:00:46 -0500

    3e cours

--- script.js
+++ script.js
@@ -7,27 +7,6 @@ function save(){
     localStorage.setItem("nom", nom);
     localStorage.setItem("prénom", prenom)
 }
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/* const taskInput = document.getElementById("taskInput");
-const addTaskBtn = document.getElementById("addTaskBtn");
-const taskList = document.getElementById("taskList");
-
-addTaskBtn.addEventListener("click", addTask())
-
-function addTask() {
-    const taskText = taskInput.value.trim();
-    if (taskText !== ""){
-        const listItem = document.getElementById(taskList).value;
-        listItem.textContent = taskText;
-        taskList.appendChild(listItem)
-        taskInput.value = document.createTextNode(taskText)
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Supprimer";
-        deleteBtn.addEventListener("click", () => {listItem.remove();
-        });}
-        
-    else{alert("Veuillez entrez une tâche valide");}
-;} */
 
 const taskInput = document.getElementById("taskInput") ;
 
@@ -35,16 +14,27 @@ const addTaskBtn = document.getElementById("addTask") ;
 
 const taskList = document.getElementById("taskList") ;
 
-var task = "task";
-var i = 0;
+const clearAll = document.getElementById("clearAll")
 
+var task = "undone task ";
+var done = "task done ";
+var i = 0;
+var j = 0;
 // La fonction trim() en JavaScript est utilisée pour supprimer les espaces (espaces, tabulations et
 
 //nouvelles lignes) au début et à la fin d'une chaîne.
 
- 
 
 addTaskBtn.addEventListener("click", addTask);
+
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+ 
+ function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
 
 function addTask() {
 
@@ -60,9 +50,35 @@ function addTask() {
 
     taskInput.value = "";
 
-    const deleteBtn = document.createElement("button");
+    const doneBtn = document.createElement("img");
 
-    deleteBtn.textContent = "Supprimer";
+    doneBtn.setAttribute("src", "../img/icons8-checkmark-50.png");
+    doneBtn.setAttribute('height', '18px');
+    doneBtn.setAttribute('width', '18px');
+
+    listItem.appendChild(doneBtn);
+
+    doneBtn.addEventListener("click", () => {
+        listItem.id = "taskDone"});
+
+    const editBtn = document.createElement("img");
+
+    editBtn.setAttribute("src", "../img/icons8-edit-file-50.png");
+    editBtn.setAttribute('height', '18px');
+    editBtn.setAttribute('width', '18px');
+
+    listItem.appendChild(editBtn);
+
+    editBtn.addEventListener("click", () => {
+        taskInput.value = listItem.textContent
+        listItem.remove()
+    });
+
+    const deleteBtn = document.createElement("img");
+
+    deleteBtn.setAttribute("src", "../img/icons8-delete-60.png");
+    deleteBtn.setAttribute('height', '18px');
+    deleteBtn.setAttribute('width', '18px');
 
     // La méthode JavaScript appendChild() est utilisée pour insérer un nouveau noeud ou repositionner un
 
@@ -74,14 +90,14 @@ function addTask() {
 
     listItem.remove();
 
- });
+    });
 
- }else{
 
-    alert("Veuillez entrer une tâche valide.");
-
- 
-
- }
-localStorage.setItem(task.concat(i), taskText);i++
- }
+    }else{
+        alert("Veuillez entrer une tâche valide.");
+    }
+localStorage.setItem(task.concat(i), (taskText), i++)
+clearAll.addEventListener("click", () => {
+    localStorage.clear()
+})
+}
