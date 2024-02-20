e79c3ba08a4d1971e651fee97a383e5c3c541ef9
Alexandre Tanguay <2383528@collegemv.qc.ca>
2024-02-20 16:56:04 -0500

    Test?

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,29 +1,48 @@
 ////////////////// Obtenez le modal d'inscription ///////////////////////
+let uname = document.getElementById("uname").value;
+let pswd = document.getElementById("pswd").value;
+let pswdRepeat = document.getElementById("repeatPSWD").value;
+alert(uname + "'s password is " + pswd + " " + pswdRepeat)
 function userInfo (){
 
-  // Todo
-  document.location='accueil.html';
+/*loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+
+    let storedName = localStorage.getItem('uname');
+    let storedPw = localStorage.getItem('pswd');
+
+    if (unameConnect === storedName && pswdConnect === storedPw) {
+        alert("You have successfully logged in.");*/
+        document.location='accueil.html';
+    }
+    //else{alert("Nuh uh")}
+//})
+  
+//}
+/* function register(){
+  if(pswdRepeat === pswd){
+    localStorage.setItem('uname', uname);
+    localStorage.setItem('pswd', pswd);
+  }
+} */
+// ////////////////// Obtenez le modal d'inscription ///////////////////////
+var modal = document.getElementById('id01');
+
+// // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+   }
 }
 
 
-
-// ////////////////// Obtenez le modal d'inscription ///////////////////////
-// var modal = document.getElementById('id01');
-
-// // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-
 // ///////////////// Obtenez le modal de connexion //////////////////////////
-// var modal2 = document.getElementById('id02');
+ var modal2 = document.getElementById('id02');
 
-// // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
-// window.onclick = function(event) {
-//     if (event.target == modal2) {
-//         modal.style.display = "none";
-//     }
-// }
+  //Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
+window.onclick = function(event) {
+     if (event.target == modal2) {
+         modal.style.display = "none";
+     }
+ }
